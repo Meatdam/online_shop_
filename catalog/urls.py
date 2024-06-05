b86022893c0e55650fc450f48d047e72efa9c2ea
Kuzkin Ilya <heartsong_@mail.ru>
2024-06-05 13:04:25 +0500

    Добавить измененный файл catalog/urls.py
    Добавить кеш на отображение одного продукта, на отображение страницы контактов, отображение всех продуктов

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.views import IndexListView, basket_add, basket_remove, ProductDetailView, ContactsView, ProductCreateView, \
     ProductDeleteView, ProductUpdateView
@@ -12,10 +13,10 @@ urlpatterns = [
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
     path('page/<int:page>/', IndexListView.as_view(), name='paginator'),
-    path('category/<int:category_id>/', IndexListView.as_view(), name='category'),
+    path('category/<int:category_id>/', cache_page(60)(IndexListView.as_view()), name='category'),
     path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete'),
     path('edit/<int:pk>/', ProductUpdateView.as_view(), name='edit'),
-    path('<int:pk>/', ProductDetailView.as_view(), name='single_product'),
-    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='single_product'),
+    path('contacts/', cache_page(60)(ContactsView.as_view()), name='contacts'),
     path('create/', ProductCreateView.as_view(), name='create')
 ]
