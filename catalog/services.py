99a826f98192d3b68d79936e3a7e4aac5974f7df
Kuzkin Ilya <heartsong_@mail.ru>
2024-06-05 13:03:07 +0500

    Добавить файл services.py
    Добавить две функции для работы с кешем "get_product_from_cache", "get_category_from_cache"

---
+++ catalog/services.py
@@ -0,0 +1,39 @@
+from django.core.cache import cache
+
+from catalog.models import Product, Category
+from config.settings import CACHE_ENABLED
+
+
+def get_product_from_cache():
+    """
+    Получает данные по товарам из кэша,
+    если кэш пуст - получает данные из БД.
+    """
+    if not CACHE_ENABLED:
+        return Product.objects.all()
+    key = 'product_list'
+    product = cache.get(key)
+    if product is not None:
+        return product
+    product = Product.objects.all()
+    cache.set(key, product)
+    return product
+
+
+def get_category_from_cache():
+    """
+    Получает данные по категориям из кэша,
+    если кэш пуст - получает данные из БД.
+    """
+    if not CACHE_ENABLED:
+        return Category.objects.all()
+    key = 'category_list'
+    category = cache.get(key)
+    if category is not None:
+        return category
+    category = Category.objects.all()
+    cache.set(key, category)
+    return category
+
+
+
